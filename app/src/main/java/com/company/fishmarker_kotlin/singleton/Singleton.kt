ad4048a3cf2f5d539b8f9d17c36d481955442afb
Igor755 <metlin.igor@gmail.com>
2019-10-28 16:54:51 +0200

    add_marker

--- app/src/main/java/com/company/fishmarker_kotlin/singleton/Singleton.kt
+++ app/src/main/java/com/company/fishmarker_kotlin/singleton/Singleton.kt
@@ -4,31 +4,34 @@ import android.graphics.drawable.Drawable
 import com.company.fishmarker_kotlin.modelclass.Marker
 import com.google.android.gms.maps.GoogleMap
 
-object Singleton {
-
-    var allmarkers :  ArrayList<Marker> = arrayListOf()
-    private var googlemap: GoogleMap? = null
+class Singleton {
 
 
-    fun addMarker(marker : Marker){
-        allmarkers.add(marker)
-    }
+    companion object {
 
-    fun createMarker(lat : Double?, lon : Double?, title : String?, icon : Drawable){
 
-       /* val marker : Marker  = googlemap.addMarker(new MarkerOptions()
+        @JvmStatic
+        var allmarkers: ArrayList<Marker> = arrayListOf()
+        
+        private var googlemap: GoogleMap? = null
+
+
+        fun addMarker(marker: Marker) {
+            allmarkers.add(marker)
+        }
+
+        fun createMarker(lat: Double?, lon: Double?, title: String?, icon: Drawable) {
+
+            /*val marker : Marker  = googlemap.addMarker(new MarkerOptions()
             .position(new LatLng(_lat, _lon))
             .title(title_marker)
             .icon(BitmapDescriptorFactory.fromBitmap(icon_marker)));
-
-
         markers.add(marker)*/
 
+        }
+
+
     }
-
-
-
-
 
 }
 
