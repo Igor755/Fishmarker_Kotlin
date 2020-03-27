ef6763c2cc27cf079e908d48ce8fe55d3443b149
Jonny <metlin.igor@gmail.com>
2020-03-27 17:55:55 +0200

    clean

--- app/src/main/java/com/company/fishmarker_kotlin/modelclass/BigWater.kt
+++ app/src/main/java/com/company/fishmarker_kotlin/modelclass/BigWater.kt
@@ -3,13 +3,14 @@ package com.company.fishmarker_kotlin.modelclass
 import com.company.fishmarker_kotlin.R
 
 enum class BigWater(val nameWater: String, val photoWater: Int) {
-    Type1("SEA", R.drawable.sea),
-    Type2("OCEAN", R.drawable.sea),
-    Type3("RIVER", R.drawable.sea),
-    Type4("LAKE", R.drawable.sea),
-    Type5("GULF", R.drawable.sea),
-    Type6("RESERVOIR", R.drawable.sea),
-    Type7("LADE", R.drawable.sea),
-    Type8("POND", R.drawable.sea),
-    Type9("ANOTHER", R.drawable.sea),
+
+    Type1("SEA", R.drawable.big_water_sea),
+    Type2("OCEAN", R.drawable.big_water_ocean),
+    Type3("RIVER", R.drawable.big_water_river),
+    Type4("LAKE", R.drawable.big_water_lake),
+    Type5("GULF", R.drawable.big_water_gulf),
+    Type6("RESERVOIR", R.drawable.big_water_reservoir),
+    Type7("LADE", R.drawable.big_water_lade),
+    Type8("POND", R.drawable.big_water_pond),
+    Type9("ANOTHER", R.drawable.big_water_another),
 }
